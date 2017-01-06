825855f05d1ae119ff03019c1ea36b1d54062327
Dmitriy Dubson <d.dubson@gmail.com>
2017-01-06 15:38:16 -0500

    Updated Niflist command

--- src/main/java/com/netsec/NifListCommand.java
+++ src/main/java/com/netsec/NifListCommand.java
@@ -1,7 +1,11 @@
 package com.netsec;
 
 import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.core.PcapNetworkInterface;
 import org.pcap4j.core.Pcaps;
+
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Author: ddubson
@@ -9,11 +13,15 @@ import org.pcap4j.core.Pcaps;
 public class NifListCommand implements Command {
     @Override
     public void exec(String... args) {
-        System.out.println("Network Interfaces:");
+        System.out.println("Network Interfaces on Device:");
         try {
-            Pcaps.findAllDevs().stream().forEach(ifs -> {
-                System.out.println(ifs.getName());
-            });
+            List<PcapNetworkInterface> nifs = Pcaps.findAllDevs();
+            nifs.sort(Comparator.comparing(PcapNetworkInterface::getName));
+            for(PcapNetworkInterface nif : nifs) {
+                System.out.println("Interface " + nif.getName() +
+                        (nif.getDescription() != null && !nif.getDescription().isEmpty()
+                                ? " [" + nif.getDescription() + "]" : ""));
+            }
         } catch (PcapNativeException e) {
             e.printStackTrace();
         }
